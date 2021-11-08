2179177f46a788615acf030d4380e473a05bd232
Max Mackie <maxmackie13@gmail.com>
2021-11-08 12:14:44 +0000

    fixed list bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,19 +14,20 @@ const Navbar = () => {
           </button>
         </div>
         <div className="links-container show-container">
-          <ul className="links"></ul>
-          <li>
-            <a href="#">home</a>
-          </li>
-          <li>
-            <a href="#">about</a>
-          </li>
-          <li>
-            <a href="#">contact</a>
-          </li>
-          <li>
-            <a href="#">products</a>
-          </li>
+          <ul className="links">
+            <li>
+              <a href="#">home</a>
+            </li>
+            <li>
+              <a href="#">about</a>
+            </li>
+            <li>
+              <a href="#">contact</a>
+            </li>
+            <li>
+              <a href="#">products</a>
+            </li>
+          </ul>
         </div>
         <ul className="social-icons">
           <li>

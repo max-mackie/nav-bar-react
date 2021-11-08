79e4f9a4eacff7b34719f545f3ab14707ca01acc
Max Mackie <maxmackie13@gmail.com>
2021-11-08 11:40:05 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
